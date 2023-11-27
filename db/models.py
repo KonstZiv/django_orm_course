92be67382b67b26aae77333b6070a5e7f56bf642
kos_zivenko <kos.zivenko@gmail.com>
2023-11-27 14:40:19 +0100

    before Many-to-Many Relationship

--- db/models.py
+++ db/models.py
@@ -10,8 +10,15 @@ class LiteraryFormat(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
-    format = models.ForeignKey(LiteraryFormat, on_delete=models.CASCADE, related_name="books")
+    price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2
+    )
+    format = models.ForeignKey(
+        LiteraryFormat,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
 
     def __str__(self):
         return f"{self.title} (price: {self.price}), format: {self.format.name}"
--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import init_django_orm  # noqa: F401
 
-from db.models import LiteraryFormat
+from db.models import LiteraryFormat, Book
+
+
+def init():
+    Book.objects.all().delete()
+    LiteraryFormat.objects.all().delete()
+
+    textbook = LiteraryFormat.objects.create(name="textbook")
+    novel = LiteraryFormat.objects.create(name="novel")
+
+    Book.objects.create(
+        title="Learning Python",
+        price=19.99,
+        format=textbook
+    )
+
+    Book.objects.create(
+        title="Refactoring. Improving the design of existing code",
+        price=22.00,
+        format=textbook
+    )
+
+    Book.objects.create(
+        title="Cassandra",
+        price=29.99,
+        format=novel
+    )
+
+    Book.objects.create(
+        title="Voroshilovgrad",
+        price=19.99,
+        format=novel
+    )
 
 
 def main():
-    filtered_formats = LiteraryFormat.objects.filter(
-        format="sci-fi",
-    ).delete()
-    # print(LiteraryFormat.objects.all())
-    print(filtered_formats)
+    # init()
+    novel = LiteraryFormat.objects.get(name="novel")
+    novel.delete()
 
 
 if __name__ == "__main__":
